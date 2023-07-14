1f8004d5bd5b026d6afb16f4f29c9748acf0602a
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-14 14:20:12 -0700

    added a map to the page from google maps api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { User } from "./user";
-import { Company } from "./Company";
+// import { User } from "./user";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
